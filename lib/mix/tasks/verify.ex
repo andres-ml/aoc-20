aa03cefd1adafcb4636798f1f58f8295c4abed61
Andrés ML <andres.mingorance@gmail.com>
2020-12-30 11:41:31 +0100

    feat(project structure): change `mix verify` output format

--- lib/mix/tasks/verify.ex
+++ lib/mix/tasks/verify.ex
@@ -3,15 +3,19 @@ defmodule Mix.Tasks.Verify do
   use Mix.Task
 
   def run(_args) do
-    for day <- 1..25, part <- [:one, :two] do
-      IO.write("Day #{day} part #{part}: ")
-      try do
-        Aoc20.solve(day, part)
-        IO.puts("✓")
-      rescue
-        _ in ArgumentError -> IO.puts("—")
-        _ -> IO.puts("error")
+    for day <- 1..25 do
+      IO.write("Day #{day}")
+      for part <- [:one, :two] do
+        IO.write("\t#{part}: ")
+        try do
+          Aoc20.solve(day, part)
+          IO.write("✓")
+        rescue
+          _ in ArgumentError -> IO.write("—")
+          _ -> IO.write("error")
+        end
       end
+      IO.puts("")
     end
   end
 
