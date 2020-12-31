a6d980d41dd9c74a6ca1351e098beab851abf90d
Andrés ML <andres.mingorance@gmail.com>
2020-12-31 18:52:41 +0100

    feat(): parallelize `mix verify`

--- lib/mix/tasks/verify.ex
+++ lib/mix/tasks/verify.ex
@@ -2,20 +2,26 @@ defmodule Mix.Tasks.Verify do
 
   use Mix.Task
 
+  def test_day(day, part) do
+    try do
+      Aoc20.solve(day, part)
+      "✓"
+    rescue
+      _ in [ArgumentError, UndefinedFunctionError] -> "—"
+      _ -> "error"
+    end
+  end
+
   def run(_args) do
+    tasks = for day <- 1..25,
+      part <- [:one, :two],
+      into: %{},
+      do: { {day, part}, Task.async(fn -> test_day(day, part) end) }
+
     for day <- 1..25 do
-      IO.write("Day #{day}")
-      for part <- [:one, :two] do
-        IO.write("\t#{part}: ")
-        try do
-          Aoc20.solve(day, part)
-          IO.write("✓")
-        rescue
-          _ in [ArgumentError, UndefinedFunctionError] -> IO.write("—")
-          _ -> IO.write("error")
-        end
-      end
-      IO.puts("")
+      part_one = Task.await(tasks[{day, :one}], :infinity)
+      part_two = Task.await(tasks[{day, :two}], :infinity)
+      IO.puts("Day #{day}\tone: #{part_one}\ttwo: #{part_two}")
     end
   end
 
